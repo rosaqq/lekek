d05d3eb9d4d784c02d4dc301a8661be31ac1c719
dffsa <danyfreitas100@hotmail.com>
2021-09-20 22:24:25 +0100

    command class simplification

--- src/main/java/net/sknv/game/Command.java
+++ src/main/java/net/sknv/game/Command.java
@@ -1,45 +1,64 @@
 package net.sknv.game;
 
 public enum Command {
-    SAVESCENE("savescene"),
-    LOADSCENE("loadscene"),
-    CLEARITEMS("clearitems"),
-    REMOVEITEM("removeitem"),
-    ROTATEITEM("rotateitem"),
-    ADDITEM("additem"),
-    ADDCUBES("addcubes"),
-    HELP("help");
+    SAVESCENE{
+        @Override
+        public String getCommandName() {
+            return "savescene";
+        }
+    },
+    LOADSCENE {
+        @Override
+        public String getCommandName() {
+            return "loadscene";
+        }
+    },
+    CLEARITEMS {
+        @Override
+        public String getCommandName() {
+            return "clearitems";
+        }
+    },
+    REMOVEITEM {
+        @Override
+        public String getCommandName() {
+            return "removeitem";
+        }
+    },
+    ROTATEITEM {
+        @Override
+        public String getCommandName() {
+            return "rotateitem";
+        }
+    },
+    ADDITEM {
+        @Override
+        public String getCommandName() {
+            return "additem";
+        }
+    },
+    ADDCUBES {
+        @Override
+        public String getCommandName() {
+            return "addcubes";
+        }
+    },
+    HELP {
+        @Override
+        public String getCommandName() {
+            return "help";
+        }
+    };
 
-    private String commandName;
     private String[] arguments;
 
-    Command(String string){
-        this.commandName = string;
-    };
-
-    Command(String[] input){
-        this(input[0]);
-        this.arguments = input;
-    };
-
-    public String getCommandName() {
-        return commandName;
-    }
+    public abstract String getCommandName();
 
     public String[] getArguments() {
         return arguments;
     }
 
-    public void setCommandName(String commandName) {
-        this.commandName = commandName;
-    }
-
     public void setArguments(String[] arguments) {
         this.arguments = arguments;
-    }
-
-    @Override
-    public String toString() {
-        return commandName;
     }
 }
