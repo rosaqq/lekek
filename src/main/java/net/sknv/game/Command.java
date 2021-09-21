ad0944c025b931add58a9a81923f66356d2f7184
dffsa <danyfreitas100@hotmail.com>
2021-09-21 23:00:48 +0100

    surely spaghet

--- src/main/java/net/sknv/game/Command.java
+++ src/main/java/net/sknv/game/Command.java
@@ -1,14 +1,27 @@
 package net.sknv.game;
 
+import java.util.List;
+import java.util.Optional;
+
 public enum Command {
     SAVESCENE{
         @Override
         public String getCommandName() { return "savescene"; }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(List.of("sceneName")));
+        }
     },
     LOADSCENE {
         @Override
         public String getCommandName() {
             return "loadscene";
+        }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(List.of("sceneName")));
         }
     },
     CLEARITEMS {
@@ -16,11 +29,24 @@ public enum Command {
         public String getCommandName() {
             return "clearitems";
         }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.empty();
+        }
     },
     REMOVEITEM {
         @Override
         public String getCommandName() {
             return "removeitem";
+        }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(
+                    List.of(Optional.of("objectId")),
+                    List.of())
+            );
         }
     },
     ROTATEITEM {
@@ -28,11 +54,21 @@ public enum Command {
         public String getCommandName() {
             return "rotateitem";
         }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(List.of("rotX", "rotY", "rotZ")));
+        }
     },
     ADDITEM {
         @Override
         public String getCommandName() {
             return "additem";
+        }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(List.of("meshName")));
         }
     },
     ADDCUBES {
@@ -40,17 +76,29 @@ public enum Command {
         public String getCommandName() {
             return "addcubes";
         }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.of(List.of(List.of("numberOfCubes")));
+        }
     },
     HELP {
         @Override
         public String getCommandName() {
             return "help";
         }
+
+        @Override
+        public Optional<List<List<Object>>> getSyntax() {
+            return Optional.empty();
+        }
     };
 
     private String[] arguments;
 
     public abstract String getCommandName();
+
+    public abstract Optional<List<List<Object>>> getSyntax();
 
     public String[] getArguments() {
         return arguments;
